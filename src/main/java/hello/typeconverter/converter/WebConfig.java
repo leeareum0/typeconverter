45fc5ed25283e0869c9864afab792d794491003b
이아름 <areum6321@gmail.com>
2024-07-29 21:05:09 +0900

    Formatter - 웹 애플리케이션 적용

--- src/main/java/hello/typeconverter/converter/WebConfig.java
+++ src/main/java/hello/typeconverter/converter/WebConfig.java
@@ -1,5 +1,6 @@
 package hello.typeconverter.converter;
 
+import hello.typeconverter.formatter.MyNumberFormatter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -11,9 +12,13 @@ public class WebConfig implements WebMvcConfigurer {
     public void addFormatters(FormatterRegistry registry) {
 
         // 등록 - 스프링 내부에서 사용하는 ConversionService에 컨버터가 추가된다.
-        registry.addConverter(new StringToIntegerConverter());
-        registry.addConverter(new IntergerToStringConverter());
+        //주석처리 우선순위
+//        registry.addConverter(new StringToIntegerConverter());
+//        registry.addConverter(new IntergerToStringConverter());
         registry.addConverter(new StringToIpPortConverter());
         registry.addConverter(new IpportToStringConverter());
+
+        //추가
+        registry.addFormatter(new MyNumberFormatter());
     }
 }
